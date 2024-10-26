ecee1017a0812e0cd76f28164b50a9ff540e6e4f
NathanL7 <nathan7@tamu.edu>
2024-10-26 11:10:18 -0500

    preliminary changes before functionality is started, organized project with folders and beginner implementation

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,92 @@
+import React, {useState} from 'react';
+import { Button } from './Button';
+import {Link} from 'react-router-dom';
+
+import './Navbar.css';
+
+// const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
+// const navbarLinks = document.querySelector('.navbar-links');
+
+// // Toggle the mobile menu visibility
+// mobileMenuIcon.addEventListener('click', () => {
+//     navbarLinks.classList.toggle('active');
+// });
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true)
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false)
+
+    //like media from css
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+          setButton(false);
+        } else {
+          setButton(true);
+        }
+      };
+    
+    //   useEffect(() => {
+    //     showButton();
+    //   }, []);
+      
+      //whenever I resize I want showButton to work for me
+      window.addEventListener('resize', showButton);
+    
+
+  return (
+    <>
+    <nav className="navbar">
+        <div className="navbar-container">
+            <Link to = "/" className = "navbar-logo">
+                <img src = "/images/pekingslogo.png"></img>
+            </Link>
+            <div className='menu-icon' onClick={handleClick}>
+                <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+            </div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/mission'
+                className='nav-links'
+                onClick={closeMobileMenu}
+                // could also be menu board if needed
+              >  
+                Our Mission
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/careers'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Careers
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to='/log-in'
+                className='nav-links-mobile'
+                onClick={closeMobileMenu}
+              >
+                Log in
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle='btn--outline'>Log in</Button>}
+        </div>
+    </nav>
+    </>
+  );
+}
+
+export default Navbar;
---
+++ frontend/src/components/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../App.css';
+import CustomerHero from '../CustomerHero';
+
+function Home() {
+    return (
+        <>
+            <CustomerHero />
+        </>
+    );
+}
+
+export default Home;
