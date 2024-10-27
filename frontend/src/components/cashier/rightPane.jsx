8a8682c2e1ea61581784b29feaee37c941da30e5
jeremiahherm <jeremiahh@tamu.edu>
2024-10-27 18:49:51 -0500

    Initial layout design complete.

--- frontend/src/components/cashier/rightPane.jsx
+++ frontend/src/components/cashier/rightPane.jsx
@@ -1,10 +1,51 @@
 import React from 'react';
 import './rightPane.css'
 
-function RightPane(){
+const orderItemDisplay = (label, price) => {
+    return (
+        <div className="orderItemRow">
+            <button className="orderItemRowText">
+                <div className="orderItemsText">{label}</div>
+                <div className="orderItemsPrice">${price}</div>
+            </button>
+            <button className="editBtn">E</button>
+            <button className="orderItemX">X</button>
+        </div>
+    );
+};
+
+function RightPane() {
+    let orderItems = [];
+    let subtotal = 59.99;
+    let tax = (subtotal*0.0625).toFixed(2);
+    let total = subtotal + parseFloat(tax);
+
+    for (let i = 0; i < 10; i++){
+        if(i%2 === 0){
+            orderItems.push(orderItemDisplay("Chicken", 9.99));
+        }else{
+            orderItems.push(orderItemDisplay("Beef Beef Beef Beef Beef Beef Beef", 12.99));
+        }
+        if(i%3 === 0){
+            orderItems.push(orderItemDisplay("AWESOME SAUCE", 199.99));
+        }
+    }
+
     return(
         <div className="rightRect">
-            <div className="orderNumber">Order<br />#5</div>
+            <div className="orderNumber">Order<br/>#5</div>
+            <hr className="separator"/>
+
+            <div className="orderItemsContainer">
+                {orderItems}
+            </div>
+            <hr className="separator"/>
+
+            <div className="priceContainer">
+                <div className="subtotal">Subtotal: ${subtotal}</div>
+                <div className="tax">Tax: ${tax}</div>
+                <div className="total">Total: ${total}</div>
+            </div>
         </div>
     );
 }
