864b249c7a90ce55aebf17e738c19b9a9b9eb635
Nathan <nathanmccullough10@tamu.edu>
2024-10-29 09:21:35 -0500

    Created main screen. Added buttons to side bar. Set each button to display bs data. Completed Sprint 1

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,20 +1,39 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import './app.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/pages/Home';
+// import React from 'react';
+// import Navbar from './components/Navbar';
+// import './app.css';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Home from './components/pages/Home';
+//
+//
+// const App = () => {
+//   return (
+//     <>
+//       <Router>
+//           <Navbar/>
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//         </Routes>
+//       </Router>
+//     </>
+//   );
+// };
+//
+// export default App;
+import React, { useState } from 'react';
+import Sidebar from './Sidebar';
+import Manager from './Manager';
+import './App.css';
 
-const App = () => {
-  return (
-    <>
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
-    </>
-  );
-};
+function App() {
+    const [selectedSection, setSelectedSection] = useState('Inventory');
+        return (
+
+            <div className="app-container">
+                <Sidebar onSelect={setSelectedSection}/>
+                <Manager selectedSection={selectedSection}/>
+            </div>
+
+        );
+}
 
 export default App;
--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../../App.css';
-import CustomerHero from '../CustomerHero';
+/*import CustomerHero from '../CustomerHero';*/
+import Manager from '../../Manager.jsx';
 
 function Home() {
     return (
         <>
-            <CustomerHero />
+            <Manager />
         </>
     );
 }
