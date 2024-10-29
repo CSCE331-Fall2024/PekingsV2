c6bb0baed21bc29f79e3d4a5f3ef54d01069fd0a
Nathan <nathanmccullough10@tamu.edu>
2024-10-29 09:23:22 -0500

    Removed error in Home.jsx from CustomerHero import.

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import '../../App.css';
-/*import CustomerHero from '../CustomerHero';*/
-import Manager from '../../Manager.jsx';
+import CustomerHero from '../CustomerHero';
+/*import Manager from '../../Manager.jsx';*/
 
 function Home() {
     return (
         <>
-            <Manager />
+            <CustomerHero />
         </>
     );
 }
