3b668864e356f7b5666a71c2584ccb6703c234a3
ceeedric <fabiocedric@gmail.com>
2024-11-03 16:42:41 -0600

    Add, update and remove menu item

--- backend/src/main/java/com/pekings/pos/controller/MenuItemController.java
+++ backend/src/main/java/com/pekings/pos/controller/MenuItemController.java
@@ -9,8 +9,12 @@ import com.pekings.pos.util.DateUtil;
 import com.pekings.pos.util.SaleItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -60,6 +64,57 @@ public class MenuItemController {
     @GetMapping("/category/{category}")
     public List<MenuItem> getMenuItemsCategory(@PathVariable("category") String category) {
         return menuItemRepository.findByCategory(category);
+    }
+
+    @PostMapping("/add")
+    public MenuItem addMenuItem(@RequestBody MenuItem menuItem) {
+        if (menuItem.getIngredients() != null) {
+            menuItem.getIngredients().forEach(ingredient -> ingredient.setMenuItem(menuItem));
+        }
+
+        return menuItemRepository.save(menuItem);
+    }
+
+    /**
+     * updates a menuItem. Sample body:
+     * **You must specify the menu item id when using this method**
+     * {
+     *     "id": 46,
+     *     "name": "Sushi Platter",
+     *     "price": 20.00,
+     *     "active": true,
+     *     "category": "seasonal",
+     *     "ingredients": [
+     *         {
+     *             "ingredient": { "id": 5 },
+     *             "amount": 3
+     *         },
+     *         {
+     *             "ingredient": { "id": 6 },
+     *             "amount": 3
+     *         }
+     *     ]
+     * }
+     *
+     * @param menuItem The menu item to update
+     * @return The menu item with updated contents
+     */
+    @PatchMapping("/update")
+    public MenuItem updateMenuItemActiveStatus(@RequestBody MenuItem menuItem) {
+
+        if (menuItem.getId() == null)
+            return null;
+
+        if (menuItem.getIngredients() != null) {
+            menuItem.getIngredients().forEach(ingredient -> ingredient.setMenuItem(menuItem));
+        }
+
+        return menuItemRepository.save(menuItem);
+    }
+
+    @PostMapping("/delete")
+    public void deleteMenuItem(@RequestBody int menuItemID) {
+        menuItemRepository.findById(menuItemID).ifPresent(menuItem -> menuItemRepository.delete(menuItem));
     }
 
 }
--- backend/src/main/java/com/pekings/pos/repository/MenuItemRepository.java
+++ backend/src/main/java/com/pekings/pos/repository/MenuItemRepository.java
@@ -2,7 +2,10 @@ package com.pekings.pos.repository;
 
 import com.pekings.pos.entities.MenuItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
