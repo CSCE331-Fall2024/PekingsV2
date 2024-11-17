729a3c240897563f87f0c33cb7a9c5d270e325bd
jeremiahherm <jeremiahh@tamu.edu>
2024-11-16 22:27:37 -0600

    Additional kitchen staff screen additions.

--- frontend/src/Kitchen.jsx
+++ frontend/src/Kitchen.jsx
@@ -1,15 +1,18 @@
 import React, {useEffect, useState} from 'react';
 import "./Kitchen.css";
 
+let tempNum = 1;
+
 function Kitchen() {
     const [currentOrders, setCurrentOrders] = useState([]);
     const [officialMenuItems, setOfficialMenuItems] = useState([]);
+    const [ingredientNames, setIngredientNames] = useState([]);
 
     // Gets all the current active orders
     useEffect(() => {
         const fetchItems = async () => {
             try {
-                const response = await fetch("/api/orders/69350", {
+                const response = await fetch("/api/orders/69352", {
                     method: "GET",
                     headers: {
                         "Content-Type": "application/json",
@@ -18,7 +21,7 @@ function Kitchen() {
 
                 if (response.ok) {
                     const orderList = await response.json();
-                    setCurrentOrders([...currentOrders, orderList, orderList]);
+                    setCurrentOrders([...currentOrders, orderList, orderList, orderList, orderList, orderList, orderList, orderList, orderList, orderList, orderList, orderList]);
                 } else {
                     console.error("Failed to fetch orders:", response.status);
                 }
@@ -56,12 +59,59 @@ function Kitchen() {
         fetchItems();
     }, []);
 
+    useEffect(() => {
+        const fetchIngredients = async () => {
+            try {
+                const response = await fetch("/api/inventory/all", {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+
+
+                if (response.ok) {
+                    const ingredients = await response.json();
+
+                    setIngredientNames(ingredients);
+                } else {
+                    console.error("Failed to fetch ingredients:", response.status);
+                }
+
+            } catch (error) {
+                console.error('Error finding ingredient:', error);
+                alert(`Failed to find ingredient: ${error.message}`);
+            }
+        }
+
+        fetchIngredients();
+    }, []);
+
     function createItemContainer(item){
-        const officialItem = officialMenuItems.find(menuItem => menuItem.id === item.menu_item_id).name;
+        const officialItem = officialMenuItems.find(menuItem => menuItem.id === item.menu_item_id);
+        const extras = item.extras;
+
+        let ingredients = [];
+        for(let i = 0; i < extras.length; i++){
+            let extra = extras[i];
+            const ingredient = ingredientNames.find(ingredientInput => ingredientInput.id === extra.ingredient_id);
+
+            if(extra.amount !== 0){
+                ingredients.push(
+                    <div className={`Ingredient-Single-Container ${extra.amount < 0 ? 'none' : 'extra'}`}>
+                        <div className="Ingredient-Name">{ingredient.name}</div>
+                        <div className="Ingredient-Amount">
+                            {extra.amount < 0 ? "None" : `Extra: ${extra.amount}`}
+                        </div>
+                    </div>
+                );
+            }
+        }
 
         return (
             <div className="Item-Container">
-                {officialItem}
+                {officialItem.name}
+                <div className="Ingredients-Container">{ingredients}</div>
             </div>
         )
     }
@@ -74,10 +124,12 @@ function Kitchen() {
             orderItemsContainers.push(createItemContainer(orderItems[i]));
         }
 
+
         return (
             <div className="Kitchen-Order">
                 <div className="Order-Header">
                     <div className="Order-Number">ID: {order.order_id}</div>
+                    <div className="Order-Timer">{tempNum++}</div>
                 </div>
 
                 <div className="Order-Container">
@@ -99,8 +151,11 @@ function Kitchen() {
     }
 
     return (
-        <div className="Kitchen-Screen">
-            {orderContainers}
+        <div className="Screen-Container">
+            {/*<button className="TempBtn">Remove</button>*/}
+            <div className="Kitchen-Screen">
+                {orderContainers}
+            </div>
         </div>
     )
 }
