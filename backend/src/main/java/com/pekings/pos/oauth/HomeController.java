ba30387c44d2d698b869571d4bb1feebe9ab0932
ceeedric <fabiocedric@gmail.com>
2024-11-22 13:58:21 -0600

    Let's use OAuth instead

---
+++ backend/src/main/java/com/pekings/pos/oauth/HomeController.java
@@ -0,0 +1,14 @@
+package com.pekings.pos.oauth;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "../frontend/index.html";
+    }
+
+}
