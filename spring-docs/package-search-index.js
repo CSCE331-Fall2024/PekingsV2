873e7bdf8ab285c207362d7a423985dd1fcd5b64
cedric <fabiocedric@gmail.com>
2024-12-02 22:53:50 -0600

    Adds spring-docs for Backend documentation

---
+++ spring-docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.pekings.pos"},{"l":"com.pekings.pos.controller"},{"l":"com.pekings.pos.entities"},{"l":"com.pekings.pos.oauth"},{"l":"com.pekings.pos.repository"},{"l":"com.pekings.pos.serialization"},{"l":"com.pekings.pos.util"}];updateSearchResults();
